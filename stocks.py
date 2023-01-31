c79203f8762980a393036142fbf09b315079c8b2
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-01-31 17:55:06 -0500

    Add files via upload

---
+++ stocks.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# simple stock price app
+
+show are the stock closing price and volume of google!
+
+""")
+
+tickerSymbol='AAPL'
+tickerData=yf.Ticker(tickerSymbol)
+tickerDf=tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+st.write("""
+## closing price
+""")
+
+st.line_chart(tickerDf.Close)
+
+st.write("""
+## volume price
+""")
+st.line_chart(tickerDf.Volume)
