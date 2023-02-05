6742d32349dbc0f7980f62d12022e33aa9635a91
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-05 00:14:34 -0500

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -9,7 +9,7 @@ Enter the stock symbol to see its closing price and volume!
 
 ticker_symbol = st.text_input("Enter a stock symbol (e.g. 'AAPL' for Apple):")
 ticker_data = yf.Ticker(ticker_symbol)
-ticker_df = ticker_data.history(period='1d', start='2010-5-31', end='2020-5-31')
+ticker_df = ticker_data.history(period='1d', start='2010-5-31', end='2023-1-31')
 
 st.write("""
 ## closing price
